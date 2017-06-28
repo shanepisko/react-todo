7a05fb48dac0730fd1fdfc69a137774b04915b00
Shane Pisko <spisko@maymediagroup.com>
2017-06-28 14:46:55 -0400

    initial commit

---
+++ app/app.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+
+
+$(document).foundation();
+
+//app css
+require('style!css!sass!applicationStyles');
+
+ReactDOM.render(
+  <p>Boiler plate 3 project</p>,
+  document.getElementById('app')
+);
