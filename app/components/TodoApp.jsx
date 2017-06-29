9c3c96c9722f906a46bb6224d41c13fb615a6468
Shane Pisko <spisko@maymediagroup.com>
2017-06-29 08:22:12 -0400

    add search text filtering

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -11,7 +11,7 @@ var TodoApp = React.createClass({
     return {
       showCompleted: false,
       searchText: '',
-      todos: TodoAPI.getTodos() 
+      todos: TodoAPI.getTodos()
     };
   },
   componentDidUpdate: function () {
@@ -47,12 +47,13 @@ var TodoApp = React.createClass({
     });
   },
   render: function () {
-    var {todos} = this.state;
+    var {todos, showCompleted, searchText} = this.state;
+    var filteredTodos = TodoAPI.filterTodos(todos, showCompleted, searchText);
 
     return (
       <div>
         <TodoSearch onSearch={this.handleSearch}/>
-        <TodoList todos={todos} onToggle={this.handleToggle}/>
+        <TodoList todos={filteredTodos} onToggle={this.handleToggle}/>
         <AddTodo onAddTodo={this.handleAddTodo}/>
       </div>
     );
